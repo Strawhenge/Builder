4e12e47abfb5181c5feacba0dd0fae4d6211c378
Matt <matt_bristow@rocketmail.com>
2022-02-20 22:14:43 +0000

    added default spawner and logger

---
+++ Strawhenge.Builder.Unity/Assets/Package/Runtime/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Strawhenge.Builder.Unity
+{
+    public class Spawner : ISpawner
+    {
+        public void Despawn(GameObject prefab)
+        {
+            Object.Destroy(prefab);
+        }
+
+        public GameObject Spawn(GameObject prefab, Vector3 position, Quaternion rotation)
+        {
+            var result = Object.Instantiate(prefab);
+            result.transform.SetPositionAndRotation(position, rotation);
+            return result;
+        }
+
+        public T Spawn<T>(T prefab, Vector3 position, Quaternion rotation) where T : MonoBehaviour
+        {
+            var result = Object.Instantiate(prefab);
+            result.transform.SetPositionAndRotation(position, rotation);
+            return result;
+        }
+    }
+}
---
+++ Strawhenge.Builder.Unity/Assets/Package/Runtime/UnityLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Strawhenge.Builder.Unity
+{
+    public class UnityLogger : ILogger
+    {
+        readonly GameObject context;
+
+        public UnityLogger(GameObject context)
+        {
+            this.context = context;
+        }
+
+        public void LogError(string message) => Debug.LogError(message, context);
+
+        public void LogException(Exception exception) => Debug.LogException(exception, context);
+
+        public void LogInformation(string message) => Debug.Log(message, context);
+
+        public void LogWarning(string message) => Debug.LogWarning(message, context);
+    }
+}
