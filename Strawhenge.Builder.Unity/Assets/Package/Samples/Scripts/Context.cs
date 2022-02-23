fe586f9b5a8f2165d1c3032ae210d51b0fc71ab1
Matt <matt_bristow@rocketmail.com>
2022-02-23 19:08:26 +0000

    updated sample scene context script

--- Strawhenge.Builder.Unity/Assets/Package/Samples/Scripts/Context.cs
+++ Strawhenge.Builder.Unity/Assets/Package/Samples/Scripts/Context.cs
@@ -1,19 +1,25 @@
 using Strawhenge.Builder;
 using Strawhenge.Builder.Unity;
 using Strawhenge.Builder.Unity.BuildItems;
+using Strawhenge.Builder.Unity.BuildItems.Snapping;
 using Strawhenge.Builder.Unity.Factories;
 using Strawhenge.Builder.Unity.ScriptableObjects;
 using Strawhenge.Builder.Unity.UI;
+using System.Linq;
 using UnityEngine;
 
 public class Context : MonoBehaviour
 {
     [SerializeField] BlueprintScriptableObject blueprint;
     [SerializeField] float moveSpeed;
+    [SerializeField] float turnSpeed;
 
     BlueprintManager blueprintManager;
     BlueprintFactory blueprintFactory;
     BlueprintScriptableObject currentBlueprint;
+
+    VerticalSnap verticalSnap;
+    HorizontalSnap horizontalSnap;
 
     void Awake()
     {
@@ -33,21 +39,27 @@ public class Context : MonoBehaviour
     void Update()
     {
         SelectBlueprint();
-        ManageBlueprint();
+
+        if (verticalSnap != null)
+        {
+            HandleVerticalSnap();
+            return;
+        }
+
+        if (horizontalSnap != null)
+        {
+            HandleHorizontalSnap();
+            return;
+        }
+
+        blueprintManager.Preview.Do(blueprint =>
+        {
+            ManageBlueprintMovement(blueprint);
+            ManageBlueprintSnapping(blueprint);
+        });
 
         if (Input.GetKeyDown(KeyCode.Return))
             blueprintManager.Build();
-    }
-
-    void ManageBlueprint()
-    {
-        blueprintManager.Preview.Do(blueprint =>
-        {
-            var x = Input.GetAxis("Horizontal");
-            var y = Input.GetAxis("Vertical");
-            var direction = new Vector3(x, 0, y).normalized;
-            blueprint.Move(moveSpeed * Time.deltaTime * direction);
-        });
     }
 
     void SelectBlueprint()
@@ -65,5 +77,105 @@ public class Context : MonoBehaviour
         currentBlueprint = blueprint;
         blueprintManager.Set(
             blueprintFactory.Create(blueprint));
+    }
+
+    void ManageBlueprintMovement(IBuildItemPreview blueprint)
+    {
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            blueprint.Move(moveSpeed * Time.deltaTime * new Vector3(0, y, 0).normalized);
+            blueprint.Turn(turnSpeed * x * Time.deltaTime);
+            return;
+        }
+
+        var direction = new Vector3(x, 0, y).normalized;
+        blueprint.Move(moveSpeed * Time.deltaTime * direction);
+    }
+
+    void ManageBlueprintSnapping(IBuildItemPreview blueprint)
+    {
+        if (!Input.GetKey(KeyCode.RightShift))
+            return;
+
+        verticalSnap = blueprint.GetAvailableVerticalSnaps().FirstOrDefault();
+
+        if (verticalSnap != null)
+        {
+            verticalSnap.Snap();
+            return;
+        }
+
+        horizontalSnap = blueprint.GetAvailableHorizontalSnaps().FirstOrDefault();
+
+        if (horizontalSnap != null)
+        {
+            horizontalSnap.Snap();
+            return;
+        }
+    }
+
+    void HandleHorizontalSnap()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            horizontalSnap = null;
+            return;
+        }
+
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (Mathf.Abs(x) >= 0.1)
+                horizontalSnap.Flip();
+            return;
+        }
+
+        if (Mathf.Abs(y) > 0.1f)
+        {
+            horizontalSnap.Turn(-y * Time.deltaTime * turnSpeed);
+        }
+
+        if (Mathf.Abs(x) > 0.01f)
+        {
+            horizontalSnap.Slide(x * moveSpeed * Time.deltaTime);
+        }
+    }
+
+    void HandleVerticalSnap()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            verticalSnap = null;
+            return;
+        }
+
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (x >= 0.1)
+                verticalSnap.TurnPrevious();
+
+            if (x <= 0.1)
+                verticalSnap.TurnNext();
+
+            return;
+        }
+
+        if (Mathf.Abs(x) > 0.1f)
+        {
+            verticalSnap.Turn(x * Time.deltaTime * turnSpeed);
+        }
+
+        if (Mathf.Abs(y) > 0.01f)
+        {
+            verticalSnap.Slide(y * moveSpeed * Time.deltaTime);
+        }
     }
 }
