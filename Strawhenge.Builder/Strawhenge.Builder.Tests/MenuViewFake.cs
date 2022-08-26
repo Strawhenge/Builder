504b8df85c9308344b119e1f708d760465216c45
Matt Bristow <strawhengestudios@gmail.com>
2022-08-26 23:22:59 +0100

    added MenuViewFake for tests

---
+++ Strawhenge.Builder/Strawhenge.Builder.Tests/MenuViewFake.cs
@@ -0,0 +1,38 @@
+﻿using Strawhenge.Builder.Menu;
+using System;
+using System.Collections.Generic;
+
+namespace Strawhenge.Builder.Tests
+{
+    internal class MenuViewFake : IMenuView
+    {
+        public event Action Back;
+        public event Action<IMenuCategory> SelectCategory;
+        public event Action<IMenuItem> SelectItem;
+
+        internal bool IsShowing { get; private set; }
+
+        internal IReadOnlyList<IMenuCategory> DisplayedCategories { get; private set; }
+
+        internal IReadOnlyList<IMenuItem> DisplayedItems { get; private set; }
+
+        internal bool IsBackButtonEnabled { get; private set; }
+
+        void IMenuView.Hide() => IsShowing = false;
+
+        void IMenuView.Show() => IsShowing = true;
+
+        void IMenuView.Populate(IReadOnlyList<IMenuCategory> categories, IReadOnlyList<IMenuItem> items, bool enableBackButton)
+        {
+            DisplayedCategories = categories;
+            DisplayedItems = items;
+            IsBackButtonEnabled = enableBackButton;
+        }
+
+        internal void InvokeBack() => Back?.Invoke();
+
+        internal void InvokeSelectCategory(IMenuCategory category) => SelectCategory?.Invoke(category);
+
+        internal void InvokeSelectItem(IMenuItem item) => SelectItem?.Invoke(item);
+    }
+}
