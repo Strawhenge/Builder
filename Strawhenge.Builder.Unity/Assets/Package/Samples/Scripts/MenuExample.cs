14ba49bfb4207c43f5bf8ef6bcd0232571895ec3
Matt Bristow <strawhengestudios@gmail.com>
2022-09-01 22:12:42 +0100

    updated menu example script

--- Strawhenge.Builder.Unity/Assets/Package/Samples/Scripts/MenuExample.cs
+++ Strawhenge.Builder.Unity/Assets/Package/Samples/Scripts/MenuExample.cs
@@ -1,24 +1,27 @@
+using Strawhenge.Builder.Menu;
 using Strawhenge.Builder.Unity;
+using System;
 using UnityEngine;
 
 public class MenuExample : MonoBehaviour
 {
-    [SerializeField] string[] _categories;
-    [SerializeField] string[] _items;
-    [SerializeField] bool _enableBack;
-
     MenuView _menuView;
+    BuilderMenu _menu;
     bool _isShowing;
+    MenuCategory _mainCategory;
 
     void Awake()
     {
         _menuView = new MenuView(new UnityLogger(gameObject));
+        _menu = new BuilderMenu(_menuView);
     }
 
     void Start()
     {
         _menuView.Setup(
             FindObjectOfType<MenuScript>(includeInactive: true));
+
+        _mainCategory = CreateMainCategory();
     }
 
     void Update()
@@ -27,11 +30,42 @@ public class MenuExample : MonoBehaviour
             return;
 
         if (_isShowing)
-            _menuView.Hide();
+            _menu.Hide();
         else
-            _menuView.Show(_categories, _items, _enableBack);
+            _menu.Show(_mainCategory);
 
         _isShowing = !_isShowing;
     }
+
+    const string Wall = "Wall";
+    const string Floor = "Floor";
+    const string Structure = "Structure";
+    const string Workbench = "Workbench";
+    const string Utility = "Utility";
+    const string Chair = "Chair";
+    const string Table = "Table";
+    const string Furniture = "Furniture";
+
+    MenuCategory CreateMainCategory()
+    {
+        var wall = CreateMenuItem(Wall);
+        var floor = CreateMenuItem(Floor);
+
+        var structure = new MenuCategory(Structure, Array.Empty<MenuCategory>(), new MenuItem[] { wall, floor });
+
+        var workbench = CreateMenuItem(Workbench);
+
+        var utility = new MenuCategory(Utility, Array.Empty<MenuCategory>(), new MenuItem[] { workbench });
+
+        var chair = CreateMenuItem(Chair);
+        var table = CreateMenuItem(Table);
+
+        var furniture = new MenuCategory(Furniture, new MenuCategory[] { utility }, new MenuItem[] { chair, table });
+
+        return new MenuCategory(string.Empty, new MenuCategory[] { structure, furniture }, Array.Empty<MenuItem>());
+    }
+
+    MenuItem CreateMenuItem(string name) =>
+            new MenuItem(name, () => Debug.Log(name));
 }
 
