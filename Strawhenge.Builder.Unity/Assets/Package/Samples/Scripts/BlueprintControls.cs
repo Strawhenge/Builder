d4e7ab1b2e4177a7ddf333e271e3f7e21b0590b4
Matt Bristow <strawhengestudios@gmail.com>
2022-09-19 20:50:02 +0100

    tidy up sample scene and integrated blueprint controls with menu select

---
+++ Strawhenge.Builder.Unity/Assets/Package/Samples/Scripts/BlueprintControls.cs
@@ -0,0 +1,141 @@
+using Strawhenge.Builder.Unity.BuildItems;
+using Strawhenge.Builder.Unity.BuildItems.Snapping;
+using System.Linq;
+using UnityEngine;
+
+namespace Strawhenge.Builder.Unity
+{
+    public class BlueprintControls : MonoBehaviour
+    {
+        [SerializeField] float _moveSpeed;
+        [SerializeField] float _turnSpeed;
+
+        BlueprintManager _blueprintManager;
+
+        VerticalSnap _verticalSnap;
+        HorizontalSnap _horizontalSnap;
+
+        void Start()
+        {
+            _blueprintManager = FindObjectOfType<Context>().BlueprintManager;
+        }
+
+        void Update()
+        {
+            _blueprintManager.Preview.Do(blueprint =>
+            {
+                if (_verticalSnap != null)
+                {
+                    HandleVerticalSnap();
+                    return;
+                }
+
+                if (_horizontalSnap != null)
+                {
+                    HandleHorizontalSnap();
+                    return;
+                }
+
+                ManageBlueprintMovement(blueprint);
+                ManageBlueprintSnapping(blueprint);
+            });
+
+            if (Input.GetKeyDown(KeyCode.Return))
+                _blueprintManager.Build();
+        }
+
+        void ManageBlueprintMovement(IBuildItemPreview blueprint)
+        {
+            var x = Input.GetAxis("Horizontal");
+            var y = Input.GetAxis("Vertical");
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                blueprint.Move(_moveSpeed * Time.deltaTime * new Vector3(0, y, 0).normalized);
+                blueprint.Turn(_turnSpeed * x * Time.deltaTime);
+                return;
+            }
+
+            var direction = new Vector3(x, 0, y).normalized;
+            blueprint.Move(_moveSpeed * Time.deltaTime * direction);
+        }
+
+        void ManageBlueprintSnapping(IBuildItemPreview blueprint)
+        {
+            if (!Input.GetKey(KeyCode.RightShift))
+                return;
+
+            _verticalSnap = blueprint.GetAvailableVerticalSnaps().FirstOrDefault();
+
+            if (_verticalSnap != null)
+            {
+                _verticalSnap.Snap();
+                return;
+            }
+
+            _horizontalSnap = blueprint.GetAvailableHorizontalSnaps().FirstOrDefault();
+
+            if (_horizontalSnap != null)
+            {
+                _horizontalSnap.Snap();
+                return;
+            }
+        }
+
+        void HandleHorizontalSnap()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                _horizontalSnap = null;
+                return;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _horizontalSnap.Flip();
+                return;
+            }
+
+            var x = Input.GetAxis("Horizontal");
+            var y = Input.GetAxis("Vertical");
+
+            if (Mathf.Abs(y) > 0.1f)
+            {
+                _horizontalSnap.Turn(-y * Time.deltaTime * _turnSpeed);
+            }
+
+            if (Mathf.Abs(x) > 0.01f)
+            {
+                _horizontalSnap.Slide(x * _moveSpeed * Time.deltaTime);
+            }
+        }
+
+        void HandleVerticalSnap()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                _verticalSnap = null;
+                return;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _verticalSnap.TurnNext();
+                return;
+            }
+
+            var x = Input.GetAxis("Horizontal");
+            var y = Input.GetAxis("Vertical");
+
+            if (Mathf.Abs(x) > 0.1f)
+            {
+                _verticalSnap.Turn(x * Time.deltaTime * _turnSpeed);
+            }
+
+            if (Mathf.Abs(y) > 0.01f)
+            {
+                _verticalSnap.Slide(y * _moveSpeed * Time.deltaTime);
+            }
+        }
+    }
+}
--- Strawhenge.Builder.Unity/Assets/Package/Samples/Scripts/Context.cs
+++ Strawhenge.Builder.Unity/Assets/Package/Samples/Scripts/Context.cs
@@ -1,174 +1,63 @@
 using Strawhenge.Builder;
+using Strawhenge.Builder.Menu;
 using Strawhenge.Builder.Unity;
 using Strawhenge.Builder.Unity.BuildItems;
-using Strawhenge.Builder.Unity.BuildItems.Snapping;
 using Strawhenge.Builder.Unity.Factories;
 using Strawhenge.Builder.Unity.ScriptableObjects;
 using Strawhenge.Builder.Unity.UI;
-using System.Linq;
 using UnityEngine;
 
 public class Context : MonoBehaviour
 {
-    [SerializeField] BlueprintScriptableObject blueprint;
-    [SerializeField] float moveSpeed;
-    [SerializeField] float turnSpeed;
+    [SerializeField] BlueprintScriptableObject[] _blueprints;
 
-    BlueprintManager blueprintManager;
-    BlueprintFactory blueprintFactory;
-    BlueprintScriptableObject currentBlueprint;
+    MenuView _menuView;
+    BuilderMenu _menu;
+    MenuItemsFactory<BlueprintScriptableObject> _menuItemsFactory;
+    MainCategory _mainCategory;
+    BlueprintFactory _blueprintFactory;
 
-    VerticalSnap verticalSnap;
-    HorizontalSnap horizontalSnap;
+    public BlueprintManager BlueprintManager { get; private set; }
 
     void Awake()
     {
+        _menuItemsFactory = new MenuItemsFactory<BlueprintScriptableObject>();
+        _menuView = new MenuView(new UnityLogger(gameObject));
+        _menu = new BuilderMenu(_menuView);
+
         var logger = new UnityLogger(gameObject);
         var inventory = new ComponentInventory(logger);
         var buildItemController = new BuildItemController();
-        var recipeFactory = new RecipeFactory();      
+        var recipeFactory = new RecipeFactory();
 
-        blueprintFactory = new BlueprintFactory(recipeFactory, logger);
-        blueprintManager = new BlueprintManager(inventory, buildItemController, new NullRecipeUI())
+        _blueprintFactory = new BlueprintFactory(recipeFactory, logger);
+        
+        BlueprintManager = new BlueprintManager(inventory, buildItemController, new NullRecipeUI())
         {
             DefaultPosition = transform.position
         };
     }
 
+    void Start()
+    {
+        _menuView.Setup(
+            FindObjectOfType<MenuScript>(includeInactive: true));
+
+        _mainCategory = _menuItemsFactory.CreateMainCategory(_blueprints, selectedBlueprint =>
+        {
+            BlueprintManager.Set(
+                _blueprintFactory.Create(selectedBlueprint));
+        });
+    }
+
     void Update()
     {
-        SelectBlueprint();
-
-        blueprintManager.Preview.Do(blueprint =>
-        {
-            if (verticalSnap != null)
-            {
-                HandleVerticalSnap();
-                return;
-            }
-
-            if (horizontalSnap != null)
-            {
-                HandleHorizontalSnap();
-                return;
-            }
-
-            ManageBlueprintMovement(blueprint);
-            ManageBlueprintSnapping(blueprint);
-        });
-
-        if (Input.GetKeyDown(KeyCode.Return))
-            blueprintManager.Build();
-    }
-
-    void SelectBlueprint()
-    {
-        if (blueprint == currentBlueprint)
+        if (!Input.GetKeyUp(KeyCode.Escape))
             return;
 
-        if (blueprint == null)
-        {
-            currentBlueprint = null;
-            blueprintManager.Unset();
-            return;
-        }
-
-        currentBlueprint = blueprint;
-        blueprintManager.Set(
-            blueprintFactory.Create(blueprint));
-    }
-
-    void ManageBlueprintMovement(IBuildItemPreview blueprint)
-    {
-        var x = Input.GetAxis("Horizontal");
-        var y = Input.GetAxis("Vertical");
-
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            blueprint.Move(moveSpeed * Time.deltaTime * new Vector3(0, y, 0).normalized);
-            blueprint.Turn(turnSpeed * x * Time.deltaTime);
-            return;
-        }
-
-        var direction = new Vector3(x, 0, y).normalized;
-        blueprint.Move(moveSpeed * Time.deltaTime * direction);
-    }
-
-    void ManageBlueprintSnapping(IBuildItemPreview blueprint)
-    {
-        if (!Input.GetKey(KeyCode.RightShift))
-            return;
-
-        verticalSnap = blueprint.GetAvailableVerticalSnaps().FirstOrDefault();
-
-        if (verticalSnap != null)
-        {
-            verticalSnap.Snap();
-            return;
-        }
-
-        horizontalSnap = blueprint.GetAvailableHorizontalSnaps().FirstOrDefault();
-
-        if (horizontalSnap != null)
-        {
-            horizontalSnap.Snap();
-            return;
-        }
-    }
-
-    void HandleHorizontalSnap()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            horizontalSnap = null;
-            return;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            horizontalSnap.Flip();
-            return;
-        }
-
-        var x = Input.GetAxis("Horizontal");
-        var y = Input.GetAxis("Vertical");
-
-        if (Mathf.Abs(y) > 0.1f)
-        {
-            horizontalSnap.Turn(-y * Time.deltaTime * turnSpeed);
-        }
-
-        if (Mathf.Abs(x) > 0.01f)
-        {
-            horizontalSnap.Slide(x * moveSpeed * Time.deltaTime);
-        }
-    }
-
-    void HandleVerticalSnap()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            verticalSnap = null;
-            return;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            verticalSnap.TurnNext();
-            return;
-        }
-
-        var x = Input.GetAxis("Horizontal");
-        var y = Input.GetAxis("Vertical");
-
-        if (Mathf.Abs(x) > 0.1f)
-        {
-            verticalSnap.Turn(x * Time.deltaTime * turnSpeed);
-        }
-
-        if (Mathf.Abs(y) > 0.01f)
-        {
-            verticalSnap.Slide(y * moveSpeed * Time.deltaTime);
-        }
+        if (_menu.IsShowing)
+            _menu.Hide();
+        else
+            _menu.Show(_mainCategory);
     }
 }
