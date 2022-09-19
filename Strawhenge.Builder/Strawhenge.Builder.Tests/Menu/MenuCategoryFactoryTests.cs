3b290bf5b5c592a933339c3f118eb2543c380e5c
Matt Bristow <strawhengestudios@gmail.com>
2022-09-19 10:18:32 +0100

    added initial menu items factory structure

---
+++ Strawhenge.Builder/Strawhenge.Builder.Tests/Menu/MenuCategoryFactoryTests.cs
@@ -0,0 +1,28 @@
+﻿using Strawhenge.Builder.Menu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Strawhenge.Builder.Tests.Menu
+{
+    public class MenuCategoryFactoryTests
+    {
+        readonly MenuCategoryFactory<SampleBuildItem> _sut = new MenuCategoryFactory<SampleBuildItem>();
+
+        [Fact]
+        public void CreateEmptyCategory()
+        {
+            var category = _sut.Create(Enumerable.Empty<SampleBuildItem>());
+
+            Assert.NotNull(category);
+            Assert.Empty(category.Items);
+            Assert.Empty(category.Subcategories);
+        }
+    }
+
+    class SampleBuildItem
+    {
+    }
+}
---
+++ Strawhenge.Builder/Strawhenge.Builder/Menu/MenuCategoryFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strawhenge.Builder.Menu
+{
+    public class MenuCategoryFactory<T>
+    {
+        public MenuCategory Create(IEnumerable<T> items)
+        {
+            return new MenuCategory(string.Empty, Array.Empty<MenuCategory>(), Array.Empty<MenuItem>());
+        }
+    }
+}
