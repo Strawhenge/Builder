75a4f40c034825313c77aeea464a9ff6691308bd
Matt Bristow <strawhengestudios@gmail.com>
2022-09-26 18:56:06 +0100

    added scrap value object

---
+++ Strawhenge.Builder/Strawhenge.Builder/ComponentInventoryExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace Strawhenge.Builder
+{
+    public static class ComponentInventoryExtensions
+    {
+        public static void AddComponent(this IComponentInventory inventory, ComponentQuantity componentQuantity) =>
+            inventory.AddComponent(componentQuantity.Component, componentQuantity.Quantity);
+    }
+}
---
+++ Strawhenge.Builder/Strawhenge.Builder/ScrapValue.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Strawhenge.Builder
+{
+    public class ScrapValue
+    {
+        readonly ComponentQuantity[] _components;
+
+        public ScrapValue(IEnumerable<ComponentQuantity> components)
+        {
+            _components = components.ToArray();
+        }
+
+        public void AddComponentsTo(IComponentInventory inventory)
+        {
+            foreach (var component in _components)
+                inventory.AddComponent(component);
+        }
+    }
+}
