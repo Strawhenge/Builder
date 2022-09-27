17bd096499c1d9bd49efa400469926bc6e2e6182
Matt Bristow <strawhengestudios@gmail.com>
2022-09-27 12:56:18 +0100

    updated component class, added equality checks

--- Strawhenge.Builder/Strawhenge.Builder.Tests/UnitTests/Component/Component_Tests.cs
+++ Strawhenge.Builder/Strawhenge.Builder.Tests/UnitTests/Component/Component_Tests.cs
@@ -3,28 +3,8 @@ using Xunit;
 
 namespace Strawhenge.Builder.Tests.UnitTests
 {
-    public class Component_Tests
+    public partial class Component_Tests
     {
-        [Theory]
-        [MemberData(nameof(MatchingComponentNames))]
-        public void Is_ShouldBeTrue(string identifier1, string identifier2)
-        {
-            var component1 = new Component(identifier1);
-            var component2 = new Component(identifier2);
-
-            Assert.True(component1.Is(component2));
-        }
-
-        [Theory]
-        [MemberData(nameof(NonMatchingComponentNames))]
-        public void Is_ShouldBeFalse(string identifier1, string identifier2)
-        {
-            var component1 = new Component(identifier1);
-            var component2 = new Component(identifier2);
-
-            Assert.False(component1.Is(component2));
-        }
-
         [Theory]
         [InlineData(null, "")]
         [InlineData("", "")]
@@ -36,21 +16,6 @@ namespace Strawhenge.Builder.Tests.UnitTests
 
             Assert.Equal(expected, component.Identifier);
         }
-
-        public static IEnumerable<object[]> MatchingComponentNames => new object[][]
-        {
-            Case("", null),
-            Case("", ""),
-            Case("Wood", "Wood"),
-            Case("Wood", "wood"),
-            Case("Wood", "Wood ")
-        };
-
-        public static IEnumerable<object[]> NonMatchingComponentNames => new object[][]
-        {
-            Case("Wood", null),
-            Case("Wood", "Metal")
-        };
 
         static object[] Case(params object[] objects) => objects;
     }
--- Strawhenge.Builder/Strawhenge.Builder/Component.cs
+++ Strawhenge.Builder/Strawhenge.Builder/Component.cs
@@ -2,8 +2,12 @@
 
 namespace Strawhenge.Builder
 {
-    public class Component
+    public sealed class Component : IEquatable<Component>
     {
+        public static bool operator ==(Component first, Component second) => first.Equals(second);
+
+        public static bool operator !=(Component first, Component second) => !first.Equals(second);
+
         public Component(string identifier)
         {
             Identifier = string.IsNullOrWhiteSpace(identifier)
@@ -15,5 +19,11 @@ namespace Strawhenge.Builder
 
         public bool Is(Component component) =>
             Identifier.Equals(component.Identifier, StringComparison.OrdinalIgnoreCase);
+
+        public bool Equals(Component other) => Is(other);
+
+        public override bool Equals(object obj) => obj is Component other && Equals(other);
+
+        public override int GetHashCode() => Identifier.GetHashCode();
     }
 }
