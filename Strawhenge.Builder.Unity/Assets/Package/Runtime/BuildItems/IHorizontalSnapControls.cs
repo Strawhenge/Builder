d792ad76399e54cd0072d0c62515d3a37e4a1086
Matt Bristow <strawhengestudios@gmail.com>
2022-10-13 23:06:31 +0100

    added horizontal snap controls and updated sample scene

---
+++ Strawhenge.Builder.Unity/Assets/Package/Runtime/BuildItems/IHorizontalSnapControls.cs
@@ -0,0 +1,15 @@
+﻿using Strawhenge.Builder.Unity.BuildItems.Snapping;
+using System;
+
+namespace Strawhenge.Builder.Unity.BuildItems
+{
+    public interface IHorizontalSnapControls
+    {
+        event Action Place;
+        event Action Release;
+
+        void ControlOn(HorizontalSnap snap);
+
+        void ControlOff();
+    }
+}
---
+++ Strawhenge.Builder.Unity/Assets/Package/Samples/Scripts/HorizontalSnapControls.cs
@@ -0,0 +1,70 @@
+using Strawhenge.Builder.Unity.BuildItems;
+using Strawhenge.Builder.Unity.BuildItems.Snapping;
+using System;
+using UnityEngine;
+
+namespace Strawhenge.Builder.Unity
+{
+    public class HorizontalSnapControls : MonoBehaviour, IHorizontalSnapControls
+    {
+        [SerializeField] float _turnSpeed;
+        [SerializeField] float _moveSpeed;
+
+        HorizontalSnap _snap;
+
+        public event Action Place;
+        public event Action Release;
+
+        public void ControlOff()
+        {
+            enabled = false;
+            _snap = null;
+        }
+
+        public void ControlOn(HorizontalSnap snap)
+        {
+            _snap = snap;
+            enabled = true;
+        }
+
+        void OnEnable()
+        {
+            if (_snap == null)
+                enabled = false;
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Release?.Invoke();
+                return;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                Place?.Invoke();
+                return;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _snap.Flip();
+                return;
+            }
+
+            var x = Input.GetAxis("Horizontal");
+            var y = Input.GetAxis("Vertical");
+
+            if (Mathf.Abs(y) > 0.1f)
+            {
+                _snap.Turn(-y * Time.deltaTime * _turnSpeed);
+            }
+
+            if (Mathf.Abs(x) > 0.01f)
+            {
+                _snap.Slide(x * _moveSpeed * Time.deltaTime);
+            }
+        }
+    }
+}
