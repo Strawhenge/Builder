eb61ad621777afb74b76d9d454a5ada5215393c9
Matt Bristow <strawhengestudios@gmail.com>
2022-10-15 21:44:29 +0100

    fix warnings

--- Strawhenge.Builder.Unity/Assets/Package/Runtime/Data/SerializableComponentQuantity.cs
+++ Strawhenge.Builder.Unity/Assets/Package/Runtime/Data/SerializableComponentQuantity.cs
@@ -1,12 +1,18 @@
 ﻿using Strawhenge.Builder.Unity.ScriptableObjects;
 using System;
+using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Strawhenge.Builder.Unity.Data
 {
     [Serializable]
     public class SerializableComponentQuantity
     {
-        public ComponentScriptableObject Component;
-        public int Quantity;
+        [SerializeField] ComponentScriptableObject _component;
+        [SerializeField, Min(1)] int _quantity;
+
+        public ComponentScriptableObject Component => _component;
+
+        public int Quantity => _quantity;
     }
 }
