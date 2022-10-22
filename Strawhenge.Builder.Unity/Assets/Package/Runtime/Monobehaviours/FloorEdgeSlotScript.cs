43c67d3e4d0f391bca5074b62484b97b9bc5a732
Matt Bristow <strawhengestudios@gmail.com>
2022-10-22 01:03:23 +0100

    extracted base slot script

--- Strawhenge.Builder.Unity/Assets/Package/Runtime/Monobehaviours/FloorEdgeSlotScript.cs
+++ Strawhenge.Builder.Unity/Assets/Package/Runtime/Monobehaviours/FloorEdgeSlotScript.cs
@@ -4,24 +4,10 @@ namespace Strawhenge.Builder.Unity
 {
     [RequireComponent(typeof(Rigidbody))]
     [RequireComponent(typeof(Collider))]
-    public class FloorEdgeSlotScript : MonoBehaviour
+    public class FloorEdgeSlotScript : BaseSlotScript
     {
-        [SerializeField] Transform _snapSlotAnchor;
         [SerializeField] bool _canFlip;
 
-        public Transform SnapSlotAnchor => _snapSlotAnchor == null
-            ? transform
-            : _snapSlotAnchor;
-
         public bool CanFlip => _canFlip;
-
-        void Awake()
-        {
-            var rigidBody = GetComponent<Rigidbody>();
-            rigidBody.isKinematic = true;
-
-            foreach (var collider in GetComponents<Collider>())
-                collider.isTrigger = true;
-        }
     }
 }
--- Strawhenge.Builder.Unity/Assets/Package/Runtime/Monobehaviours/WallSideSlotScript.cs
+++ Strawhenge.Builder.Unity/Assets/Package/Runtime/Monobehaviours/WallSideSlotScript.cs
@@ -1,24 +1,6 @@
-using UnityEngine;
-
 namespace Strawhenge.Builder.Unity
 {
-    [RequireComponent(typeof(Rigidbody))]
-    [RequireComponent(typeof(Collider))]
-    public class WallSideSlotScript : MonoBehaviour
+    public class WallSideSlotScript : BaseSlotScript
     {
-        [SerializeField] Transform _snapSlotAnchor;
-
-        public Transform SnapSlotAnchor => _snapSlotAnchor == null
-            ? transform
-            : _snapSlotAnchor;
-
-        void Awake()
-        {
-            var rigidBody = GetComponent<Rigidbody>();
-            rigidBody.isKinematic = true;
-
-            foreach (var collider in GetComponents<Collider>())
-                collider.isTrigger = true;
-        }
     }
 }
