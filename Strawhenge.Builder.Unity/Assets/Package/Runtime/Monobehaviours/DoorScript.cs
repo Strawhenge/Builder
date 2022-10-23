5b98e76bef472d73b07d7ba7ff78c57a1feb9f3d
Matt Bristow <strawhengestudios@gmail.com>
2022-10-23 18:19:25 +0100

    fix hinge position when door moves

--- Strawhenge.Builder.Unity/Assets/Package/Runtime/Monobehaviours/DoorScript.cs
+++ Strawhenge.Builder.Unity/Assets/Package/Runtime/Monobehaviours/DoorScript.cs
@@ -3,19 +3,27 @@ using UnityEngine;
 namespace Strawhenge.Builder.Unity.Monobehaviours
 {
     [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(HingeJoint))]
     public class DoorScript : MonoBehaviour
     {
         Transform _transform;
         Rigidbody _rigidbody;
+        HingeJoint _hinge;
+        
         Quaternion _resetRotation;
         Vector3 _resetPosition;
+        Vector3 _resetHingeAnchorPosition;
 
         void Awake()
         {
             _transform = transform;
             _resetRotation = _transform.localRotation;
             _resetPosition = _transform.localPosition;
+
             _rigidbody = GetComponent<Rigidbody>();
+
+            _hinge = GetComponent<HingeJoint>();
+            _resetHingeAnchorPosition = _hinge.connectedAnchor;
         }
 
         [ContextMenu("Lock")]
@@ -29,6 +37,7 @@ namespace Strawhenge.Builder.Unity.Monobehaviours
         [ContextMenu("Unlock")]
         public void Unlock()
         {
+            _hinge.connectedAnchor = _resetHingeAnchorPosition;
             _rigidbody.isKinematic = false;
         }
     }
