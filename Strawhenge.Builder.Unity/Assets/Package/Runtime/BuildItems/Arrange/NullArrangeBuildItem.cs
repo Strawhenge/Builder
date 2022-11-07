6a6d41ff0033ed048ccd6c0ecf6a7ebf4839f468
Matt Bristow <strawhengestudios@gmail.com>
2022-11-07 22:55:56 +0000

    disable event not used warning on null object

--- Strawhenge.Builder.Unity/Assets/Package/Runtime/BuildItems/Arrange/NullArrangeBuildItem.cs
+++ Strawhenge.Builder.Unity/Assets/Package/Runtime/BuildItems/Arrange/NullArrangeBuildItem.cs
@@ -8,8 +8,10 @@ namespace Strawhenge.Builder.Unity.BuildItems
 {
     public class NullArrangeBuildItem : IArrangeBuildItem
     {
+#pragma warning disable 67
         public event Action ClippingChanged;
-        
+#pragma warning restore 67
+
         public Vector3 Position { get; } = Vector3.zero;
 
         public Quaternion Rotation { get; } = new Quaternion();
