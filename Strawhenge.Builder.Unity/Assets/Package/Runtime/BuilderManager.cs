636ee72a9ff41464e78f24a75eb1961167c86891
Matt Bristow <strawhengestudios@gmail.com>
2022-11-19 17:35:55 +0000

    initial work on BuilderManager with camera layer toggles

---
+++ Strawhenge.Builder.Unity/Assets/Package/Runtime/BuilderManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace Strawhenge.Builder.Unity
+{
+    public class BuilderManager
+    {
+        readonly Camera _camera;
+        readonly ILayersAccessor _layersAccessor;
+
+        public BuilderManager(Camera camera, ILayersAccessor layersAccessor)
+        {
+            _camera = camera;
+            _layersAccessor = layersAccessor;
+        }
+
+        public void On()
+        {
+            MarkerLayersOn();
+        }
+
+        public void Off()
+        {
+            MarkerLayersOff();
+        }
+
+        void MarkerLayersOn()
+        {
+            foreach (var layer in _layersAccessor.MarkerLayers)
+            {
+                _camera.cullingMask |= 1 << layer;
+            }
+        }
+
+        void MarkerLayersOff()
+        {
+            foreach (var layer in _layersAccessor.MarkerLayers)
+            {
+                _camera.cullingMask &= ~(1 << layer);
+            }
+        }
+    }
+
+    public interface ILayersAccessor
+    {
+        int[] MarkerLayers { get; }
+    }
+}
---
+++ Strawhenge.Builder.Unity/Assets/Package/Tests/UnitTests/BuilderManagerTests.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using UnityEngine;
+
+namespace Strawhenge.Builder.Unity.Tests
+{
+    public class BuilderManagerTests
+    {
+        const int EnvironmentLayer = 1;
+        static readonly int[] MarkerLayers = { 2, 3, 4 };
+
+        static readonly ILayersAccessor LayersAccessor = new LayersFake()
+        {
+            MarkerLayers = MarkerLayers
+        };
+
+        [Test]
+        public void Builder_markers_layers_should_be_visible_when_builder_is_enabled()
+        {
+            var camera = SetUpCamera();
+            var sut = new BuilderManager(camera, LayersAccessor);
+
+            sut.On();
+
+            Assert.True(
+                AllMarkersVisible(camera));
+        }
+
+        [Test]
+        public void Builder_markers_layers_should_not_be_visible_when_builder_is_disabled()
+        {
+            var camera = SetUpCamera();
+            var sut = new BuilderManager(camera, LayersAccessor);
+
+            sut.On();
+            sut.Off();
+
+            Assert.True(
+                AllMarkersNotVisible(camera));
+        }
+
+        static bool AllMarkersVisible(Camera camera) => MarkerLayers
+            .All(layer => ((camera.cullingMask & (1 << layer)) != 0));
+
+        static bool AllMarkersNotVisible(Camera camera) => MarkerLayers
+            .All(layer => ((camera.cullingMask & (1 << layer)) == 0));
+
+        static Camera SetUpCamera()
+        {
+            var camera = new GameObject().AddComponent<Camera>();
+
+            camera.cullingMask = EnvironmentLayer;
+            return camera;
+        }
+    }
+
+    class LayersFake : ILayersAccessor
+    {
+        public int[] MarkerLayers { get; set; }
+    }
+}
