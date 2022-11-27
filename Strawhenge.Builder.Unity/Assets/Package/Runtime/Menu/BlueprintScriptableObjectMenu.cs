05a2518a23e00d9043625f0a9f79fe2150324795
Matt Bristow <strawhengestudios@gmail.com>
2022-11-27 15:37:27 +0000

    implemented BlueprintScriptableObjectMenu

---
+++ Strawhenge.Builder.Unity/Assets/Package/Runtime/Menu/BlueprintScriptableObjectMenu.cs
@@ -0,0 +1,31 @@
+﻿using Strawhenge.Builder.Menu;
+using Strawhenge.Builder.Unity.ScriptableObjects;
+using System;
+
+namespace Strawhenge.Builder.Unity
+{
+    public class BlueprintScriptableObjectMenu : IBlueprintScriptableObjectMenu
+    {
+        readonly BuilderMenu _menu;
+        readonly MainCategory _mainCategory;
+
+        public BlueprintScriptableObjectMenu(
+            BuilderMenu menu,
+            MenuItemsFactory<BlueprintScriptableObject> menuItemsFactory,
+            IBlueprintRepository blueprints)
+        {
+            _menu = menu;
+            _menu.Exited += () => Exit?.Invoke();
+
+            _mainCategory = menuItemsFactory
+                .CreateMainCategory(blueprints.GetAll(), blueprint => Select?.Invoke(blueprint));
+        }
+
+        public event Action<BlueprintScriptableObject> Select;
+        public event Action Exit;
+
+        public void Open() => _menu.Show(_mainCategory);
+
+        public void Close() => _menu.Hide();
+    }
+}
---
+++ Strawhenge.Builder.Unity/Assets/Package/Runtime/Menu/IBlueprintRepository.cs
@@ -0,0 +1,10 @@
+﻿using Strawhenge.Builder.Unity.ScriptableObjects;
+using System.Collections.Generic;
+
+namespace Strawhenge.Builder.Unity
+{
+    public interface IBlueprintRepository
+    {
+        IReadOnlyList<BlueprintScriptableObject> GetAll();
+    }
+}
