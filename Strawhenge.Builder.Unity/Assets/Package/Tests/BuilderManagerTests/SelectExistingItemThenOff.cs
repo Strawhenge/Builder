7521b7293392b346ae8abcebbf5c7ed00b9ff3a9
Matt Bristow <strawhengestudios@gmail.com>
2022-12-16 21:15:01 +0000

    added more test cases to expose some bugs

---
+++ Strawhenge.Builder.Unity/Assets/Package/Tests/BuilderManagerTests/SelectExistingItemThenOff.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework;
+
+namespace Strawhenge.Builder.Unity.Tests.BuilderManagerTests
+{
+    public class SelectExistingItemThenOff : BaseBuilderManagerTest
+    {
+        protected override void Act()
+        {
+            Sut.On();
+            InvokeExistingItemSelected();
+            Sut.Off();
+        }
+        
+        [Test]
+        public void Build_item_selector_should_disable()
+        {
+            Assert.False(IsExistingBuildItemSelectorEnabled());
+        }
+
+        [Test]
+        public void Should_disable_builder_manager_UI()
+        {
+            Assert.False(IsBuilderManagerUIEnabled());
+        }
+        
+        [Test]
+        public void Menu_should_be_closed()
+        {
+            Assert.False(IsMenuOpen());
+        }
+    }
+}
