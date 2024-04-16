893f06742a08875220f0f6224cdad5b6839493f6
Matt Bristow <strawhengestudios@gmail.com>
2024-04-16 14:41:52 +0100

    added position and rotation to build item data

--- Strawhenge.Builder.Unity/Assets/Package/Runtime/Progress/BuildItemData.cs
+++ Strawhenge.Builder.Unity/Assets/Package/Runtime/Progress/BuildItemData.cs
@@ -1,7 +1,13 @@
+using UnityEngine;
+
 namespace Strawhenge.Builder.Unity
 {
     public class BuildItemData
     {
         public string Name { get; set; }
+
+        public Vector3 Position { get; set; }
+
+        public Vector3 Rotation { get; set; }
     }
 }
--- Strawhenge.Builder.Unity/Assets/Package/Samples/Scripts/Context.cs
+++ Strawhenge.Builder.Unity/Assets/Package/Samples/Scripts/Context.cs
@@ -101,11 +101,36 @@ public class Context : MonoBehaviour
             {
                 new BuildItemData()
                 {
-                    Name = "Wall"
+                    Name = "Floor",
+                    Position = new Vector3(2.015505790710449F, 0.005000000819563866F, 1.7324241399765015F)
                 },
                 new BuildItemData()
                 {
-                    Name = "Wall"
+                    Name = "WallWithDoor",
+                    Position = new Vector3(2.015505790710449F, 0.005000000819563866F, 1.2324241399765015F)
+                },
+                new BuildItemData()
+                {
+                    Name = "Wall",
+                    Position = new Vector3(2.515505790710449F, 0.005000000819563866F, 1.7324241399765015F),
+                    Rotation = new Quaternion(0, 0.7071067690849304F, 0, -0.70710688829422F).eulerAngles
+                },
+                new BuildItemData()
+                {
+                    Name = "Wall",
+                    Position = new Vector3(2.015505790710449F, 0.004999995231628418F, 2.23242449760437F),
+                    Rotation = new Quaternion(0, -1, 0, 1.6858736273661635e-7F).eulerAngles
+                },
+                new BuildItemData()
+                {
+                    Name = "Wall",
+                    Position = new Vector3(1.5155055522918702F, 0.004999995231628418F, 1.7324247360229493F),
+                    Rotation = new Quaternion(0, 0.7071069478988648F, 0, 0.7071065902709961F).eulerAngles
+                },
+                new BuildItemData()
+                {
+                    Name = "Floor",
+                    Position = new Vector3(1.5155055522918702F, 0.004999995231628418F, 1.7324247360229493F)
                 }
             }
         });
