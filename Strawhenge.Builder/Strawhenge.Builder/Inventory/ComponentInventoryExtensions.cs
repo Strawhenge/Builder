eb434cb492a81d9946bd5de2fedd82ad9ace3a9c
Strawhenge <strawhengestudios@gmail.com>
2026-04-17 19:31:32 +0100

    implemented AddComponents extension to inventory

--- Strawhenge.Builder/Strawhenge.Builder/Inventory/ComponentInventoryExtensions.cs
+++ Strawhenge.Builder/Strawhenge.Builder/Inventory/ComponentInventoryExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace Strawhenge.Builder
+﻿using System.Collections.Generic;
+
+namespace Strawhenge.Builder
 {
     public static class ComponentInventoryExtensions
     {
@@ -7,6 +9,14 @@
 
         public static void AddComponent(this IComponentInventory inventory, ComponentQuantity componentQuantity) =>
             inventory.AddComponent(componentQuantity.Component, componentQuantity.Quantity);
+
+        public static void AddComponents(
+            this IComponentInventory inventory,
+            IEnumerable<ComponentQuantity> components)
+        {
+            foreach (var componentQuantity in components)
+                inventory.AddComponent(componentQuantity);
+        }
 
         public static void RemoveComponent(this IComponentInventory inventory, Component component) =>
             inventory.RemoveComponent(component, 1);
