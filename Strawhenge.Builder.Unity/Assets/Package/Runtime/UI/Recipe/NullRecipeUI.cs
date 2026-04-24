b640aa2f68e09fc0cfd754b79ecde52b960dfc65
Strawhenge <strawhengestudios@gmail.com>
2026-04-24 19:07:36 +0100

    implement singleton pattern for NullRecipeUI and NullScrapUI

--- Strawhenge.Builder.Unity/Assets/Package/Runtime/UI/Recipe/NullRecipeUI.cs
+++ Strawhenge.Builder.Unity/Assets/Package/Runtime/UI/Recipe/NullRecipeUI.cs
@@ -4,6 +4,12 @@ namespace Strawhenge.Builder.Unity.UI.Recipe
 {
     public class NullRecipeUI : IRecipeUI
     {
+        public static IRecipeUI Instance { get; } = new NullRecipeUI();
+
+        NullRecipeUI()
+        {
+        }
+
         public void Show(string recipeName, IEnumerable<RecipeRequirement> requirements)
         {
         }
--- Strawhenge.Builder.Unity/Assets/Package/Runtime/UI/Scrap/NullScrapUI.cs
+++ Strawhenge.Builder.Unity/Assets/Package/Runtime/UI/Scrap/NullScrapUI.cs
@@ -3,7 +3,13 @@
 namespace Strawhenge.Builder.Unity.UI.Scrap
 {
     public class NullScrapUI : IScrapUI
-    {   
+    {
+        public static IScrapUI Instance { get; } = new NullScrapUI();
+
+        NullScrapUI()
+        {
+        }
+
         public void Show(string scrapName, IEnumerable<ScrapAddition> additions)
         {
         }
